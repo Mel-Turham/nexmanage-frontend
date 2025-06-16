a1cc04d813c4bc6df8dc9ef262ac2bb9dd0a33c7
jason-KITIO <kanamax00@gmail.com>
2025-06-16 18:14:43 +0100

    Sauvegarde avant pull

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -16,9 +16,7 @@ import {
 } from "@/components/ui/sidebar";
 import { TeamSwitcher } from "./team-switcher";
 import {
-
   Analytics02Icon,
-
   Calendar02Icon,
   CursorPointer02Icon,
   Home01Icon,
@@ -27,6 +25,7 @@ import {
   UserListIcon,
   WorkHistoryIcon,
 } from "hugeicons-react";
+import Link from "next/link";
 
 const data = {
   teams: [{ name: "Acme Inc" }, { name: "Acme Corp." }, { name: "Evil Corp." }],
@@ -36,7 +35,7 @@ const data = {
       url: "#",
       items: [
         { title: "Accueil", icon: Home01Icon, url: "/admin" },
-        { title: "Planning", icon: Calendar02Icon, url: "/admin/presences" },
+        { title: "Planning", icon: Calendar02Icon, url: "/admin/planning" },
         { title: "Congés", icon: WorkHistoryIcon, url: "/admin/conges" },
       ],
     },
@@ -71,7 +70,7 @@ const data = {
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   const pathname = usePathname();
 
-  console.log(pathname)
+  console.log(pathname);
 
   return (
     <Sidebar {...props}>
@@ -88,13 +87,12 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                   const Icon = item.icon;
                   // Détermine si l'item est actif en comparant avec la route courante
                   // On peut utiliser startsWith si tu veux activer aussi les sous-routes
-                  const isActive =
-                    pathname === item.url
+                  const isActive = pathname === item.url;
 
                   return (
                     <SidebarMenuItem key={item.title}>
                       <SidebarMenuButton asChild>
-                        <a
+                        <Link
                           href={item.url}
                           className={`flex items-center space-x-2 rounded-none px-2 py-3 h-fit transition-colors ${
                             isActive
@@ -109,7 +107,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                             />
                           )}
                           <span>{item.title}</span>
-                        </a>
+                        </Link>
                       </SidebarMenuButton>
                     </SidebarMenuItem>
                   );
