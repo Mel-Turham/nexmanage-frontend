f599da9e13590c5a868a9bcc3d288800e66319db
jason-KITIO <kanamax00@gmail.com>
2025-06-17 13:30:53 +0100

    Refactor employer components and update data structure for improved clarity and functionality

--- components/header.tsx
+++ components/header.tsx
@@ -12,34 +12,22 @@ const Header = () => {
     },
   };
   return (
-    <header className="flex h-[10vh] shrink-0 items-center gap-2 px-4">
-      <div className="flex flex-row items-center px-2 rounded-md border border-gray w-full">
-        <Search01Icon color="#e5e5e5" />
+    <header className="flex flex-row justify-between h-[10vh] shrink-0 items-center gap-2 px-4">
+      <div className="flex flex-row items-center px-2 rounded-md border border-gray w-[40%] focus-within:shadow-md focus-within:border-blue-500 transition-all duration-200">
+        <Search01Icon color="#e5e5e5" size={24} />
         <input
-          type="search "
+          type="search"
           name="search"
           placeholder="Recherche un employer, un contrat,..."
-          className="py-2 px-4 w-full outline-0"
+          className="py-2 px-4 w-full outline-none"
         />
       </div>
-      <Button size={"icon"} variant={"outline"}>
-        <Notification02Icon />
-      </Button>
-      <NavUser user={data.user} />
-      {/* <div className="flex flex-row items-center px-3 w-fit">
-            <Image
-              src={"/user.png"}
-              alt="photo de profil"
-              width={1000}
-              height={1000}
-              className="h-full w-10"
-            />
-            <div className="flex flex-col items-start">
-              <h1 className="font-semibold text-lg">Username</h1>
-              <p className="text-gray text-sm">696 696 696</p>
-            </div>
-            <ArrowDown01Icon />
-          </div> */}
+      <div className="flex flex-row items-center gap-2">
+        <Button size={"icon"} variant={"outline"}>
+          <Notification02Icon />
+        </Button>
+        <NavUser user={data.user} />
+      </div>
     </header>
   );
 };
