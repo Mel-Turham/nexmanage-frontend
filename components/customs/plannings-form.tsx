97ac5fad86d63d77c2b3497ec1cbd49de9545015
jason-KITIO <kanamax00@gmail.com>
2025-06-17 14:16:07 +0100

    refactor: standardize import statements and improve component structure in modal and planning form

--- components/customs/plannings-form.tsx
+++ components/customs/plannings-form.tsx
@@ -1,61 +1,63 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import CreateModal from './modals/create-contrat-modal';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import ContratForm from './form-contrat';
+import React, { useState } from "react";
+import CreateModal from "./modals/create-contrat-modal";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import ContratForm from "./form-contrat";
+import { AddCircleIcon } from "hugeicons-react";
 
 const PlanningForm = () => {
   const [open, setOpen] = useState<boolean>(false);
 
   return (
-    <div className='relative'>
+    <div className="relative">
       <button
-        className='bg-[#344EA2] hover:bg-[#2a3d8f] text-white py-2 px-4 rounded transition-colors cursor-pointer'
+        type="submit"
+        className="flex flex-row w-fit custom-button-gradient py-2 px-4 rounded-lg gap-2"
         onClick={() => setOpen(true)}
       >
-        Create
+        <AddCircleIcon />
+        {"Créer"}
       </button>
-
       <CreateModal open={open} setOpen={setOpen}>
-        <Tabs defaultValue='contrat' className='w-full'>
-          <TabsList className='h-auto rounded-none border-b bg-transparent p-0 w-full'>
+        <Tabs defaultValue="contrat" className="w-full">
+          <TabsList className="h-auto rounded-none border-b bg-transparent p-0 w-full">
             <TabsTrigger
-              value='contrat'
-              className='data-[state=active]:after:bg-red-400 relative rounded-none py-2 after:absolute after:inset-x-0 after:bottom-0 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none'
+              value="contrat"
+              className="data-[state=active]:after:bg-red-400 relative rounded-none py-2 after:absolute after:inset-x-0 after:bottom-0 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
             >
               Contrat
             </TabsTrigger>
             <TabsTrigger
-              value='gabarit'
-              className='data-[state=active]:after:bg-primary relative rounded-none py-2 after:absolute after:inset-x-0 after:bottom-0 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none'
+              value="gabarit"
+              className="data-[state=active]:after:bg-primary relative rounded-none py-2 after:absolute after:inset-x-0 after:bottom-0 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
             >
               Gabarit
             </TabsTrigger>
             <TabsTrigger
-              value='Récurrence'
-              className='data-[state=active]:after:bg-primary relative rounded-none py-2 after:absolute after:inset-x-0 after:bottom-0 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none'
+              value="Récurrence"
+              className="data-[state=active]:after:bg-primary relative rounded-none py-2 after:absolute after:inset-x-0 after:bottom-0 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
             >
               Récurrence
             </TabsTrigger>
             <TabsTrigger
-              value='Commentaires'
-              className='data-[state=active]:after:bg-primary relative rounded-none py-2 after:absolute after:inset-x-0 after:bottom-0 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none'
+              value="Commentaires"
+              className="data-[state=active]:after:bg-primary relative rounded-none py-2 after:absolute after:inset-x-0 after:bottom-0 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
             >
               Commentaires
             </TabsTrigger>
           </TabsList>
 
-          <TabsContent value='contrat'>
+          <TabsContent value="contrat">
             <ContratForm />
           </TabsContent>
-          <TabsContent value='gabarit'>
+          <TabsContent value="gabarit">
             <div>Gabarit</div>
           </TabsContent>
-          <TabsContent value='Récurrence'>
+          <TabsContent value="Récurrence">
             <div>Récurrence</div>
           </TabsContent>
-          <TabsContent value='Commentaires'>
+          <TabsContent value="Commentaires">
             <div>Commentaires</div>
           </TabsContent>
         </Tabs>
